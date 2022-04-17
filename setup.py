39de51f7e800d2083f5e9a3688ff5ac0bd5852f7
Vineeth Yeluru <vineethyeluru@gmail.com>
2022-04-16 19:35:50 -0500

    Updated setup package & generated new dummy datasets

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="peer_to_peer_lending",
     packages=find_packages(),
-    package_data={"data": ["dummy_data/*.csv"]},
+    package_data={"data": ["dummy_data/dummy_listings.csv", "dummy_data/dummy_loans.csv"]},
     include_package_data=True,
 )
