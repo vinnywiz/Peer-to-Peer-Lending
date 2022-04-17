dbd2ab0fab457586264b5d73f417391f723f681b
vinnywiz <vineethyeluru@gmail.com>
2022-04-17 10:29:37 -0500

    read files into pandas df

--- p2p_lend/etl/preprocess.py
+++ p2p_lend/etl/preprocess.py
@@ -1,8 +1,28 @@
 import pandas as pd
 import os
+import glob
 
 
 def preprocess_data(data_path, year_start=2011, year_end=2012):
 
-    listing_df = pd.read_csv(f'{data_path}/listings/', encoding_errors='ignore', low_memory=False)
-    loan_df = pd.read_csv(f'{data_path}/loans/', encoding_errors='ignore', low_memory=False)
+    listing_path = f'{data_path}/listings/'
+    loan_path = f'{data_path}/loans/'
+    listing_files = glob.glob(os.path.join(listing_path, "dummy*.csv"))
+    loan_files = glob.glob(os.path.join(loan_path, "dummy*.csv"))
+    print(listing_files, loan_files)
+    lis, loan = [], []
+
+    listing_df = read_files_to_pandas(listing_files)
+    loan_df = read_files_to_pandas(loan_files)
+
+    return listing_df, loan_df
+
+
+def read_files_to_pandas(filenames):
+    lis =[]
+    for filename in filenames:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        lis.append(df)
+    all_files_df = pd.concat(lis, axis=0, ignore_index=True)
+
+    return all_files_df
--- p2p_lend/etl/run_etl.py
+++ p2p_lend/etl/run_etl.py
@@ -1,10 +1,9 @@
 import logging
-from p2p_lend.etl import (
-    preprocess_data,
-    join_data,
-    smote_data,
-    feature_engineer
-)
+from .preprocess import preprocess_data
+#     join_data,
+#     smote_data,
+#     feature_engineer
+# )
 
 logger = logging.getLogger(__name__)
 
@@ -13,21 +12,22 @@ def run_etl(dummy=True, data_path='data'):
 
     if dummy:
         logger.info(f"Preprocessing loan and listing data from {data_path}/dummy_data")
-        preprocess_data(data_path=f'{data_path}/dummy_data')
+        list_df, loan_df = preprocess_data(data_path=f'{data_path}/dummy_data')
         logger.info("Preprocessing successful!")
     else:
         logger.info(f"Preprocessing loan and listing data from {data_path}/real_data")
-        preprocess_data(data_path=f'{data_path}/real_data')
+        list_df, loan_df = preprocess_data(data_path=f'{data_path}/real_data')
         logger.info("Preprocessing successful!")
 
-    logger.info("Joining loan data to listings")
-    join_data(data_path=data_path)
-
-    logger.info("Interpolating data with SMOTE")
-    smote_data(data_path=data_path)
-
-    logger.info("Running feature engineering to get model ready data")
-    feature_engineer(data_path=data_path)
+    return list_df, loan_df
+    # logger.info("Joining loan data to listings")
+    # join_data(data_path=data_path)
+    #
+    # logger.info("Interpolating data with SMOTE")
+    # smote_data(data_path=data_path)
+    #
+    # logger.info("Running feature engineering to get model ready data")
+    # feature_engineer(data_path=data_path)
 
 
 
