bbfaf706b63e2cc7080e8e8bfa04eec58083adcb
Vineeth Yeluru <vineethyeluru@gmail.com>
2022-04-17 21:19:33 -0500

    updated preprocess logic with filtering to specific columns

--- p2p_lend/etl/preprocess.py
+++ p2p_lend/etl/preprocess.py
@@ -1,20 +1,30 @@
 import pandas as pd
 import os
 import glob
+import yaml
 
-
-def preprocess_data(data_path, year_start=2011, year_end=2012):
-
-    listing_path = f'{data_path}/listings/'
-    loan_path = f'{data_path}/loans/'
+def preprocess_data(year_start=2011, year_end=2012, filter_listings=True, use_dummy=True):
+    if use_dummy == True:
+        listing_path = f'..\\..\\data\\dummy_data\\listings\\'
+        loan_path = f'..\\..\\data\\dummy_data\\loans\\'
+    else:
+        listing_path = f'..\\..\\data\\real_data\\listings\\'
+        loan_path = f'..\\..\\data\\real_data\\loans\\'        
     listing_files = glob.glob(os.path.join(listing_path, "dummy*.csv"))
     loan_files = glob.glob(os.path.join(loan_path, "dummy*.csv"))
     print(listing_files, loan_files)
-    lis, loan = [], []
-
     listing_df = read_files_to_pandas(listing_files)
     loan_df = read_files_to_pandas(loan_files)
-
+    if filter_listings:
+        config = read_yaml_config_file()
+        listing_columns = config['listing_columns']
+        listing_columns_valid = []
+        for col in listing_columns:
+            if col not in listing_df.columns:
+                print(col)
+            else:
+                listing_columns_valid.append(col)
+        listing_df = listing_df[listing_columns_valid]
     return listing_df, loan_df
 
 
@@ -26,3 +36,8 @@ def read_files_to_pandas(filenames):
     all_files_df = pd.concat(lis, axis=0, ignore_index=True)
 
     return all_files_df
+
+def read_yaml_config_file():
+    with open('column_config.yaml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
